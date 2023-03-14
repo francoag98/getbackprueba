a7ff4f7a1c0249223ae07fb0b2a1454d72060dea
Franco Aglieri <francoaglieri@hotmail.com>
2023-03-14 16:20:56 -0300

    fix

--- src/components/pokemonsCard.jsx
+++ src/components/pokemonsCard.jsx
@@ -81,69 +81,76 @@ export const PokemonsCard = () => {
   };
   if (loading) {
     return (
-      <div className="flex flex-col justify-center items-center w-screen h-screen">
+      <div className="flex flex-col justify-center items-center w-full h-screen">
         <ClipLoader color="purple" size={100} />
         <h3 className="text-purple-800 text-2xl font-bold mt-4">
           Cargando Pokemon...
         </h3>
       </div>
     );
-  }
-  return (
-    <div className="flex w-screen h-screen items-center justify-center bg-slate-100">
-      <div>
-        {pokemon && translatedPokemon ? (
-          <div className="card card-side p-6 bg-base-100 shadow-xl max-sm:flex-col w-full">
-            <figure>
-              <img src={pokemon?.image} className="w-64 ml-10" alt="Movie" />
-            </figure>
-            <div className="card-body mr-16">
-              <h1 className="card-title text-4xl ml-3 max-sm:justify-center max-sm:text-2xl">
-                {translatedPokemon.name.toUpperCase()}
-              </h1>
-              <div className="p-4 flex flex-row gap-6 max-sm:justify-center">
-                <div className="max-sm:justify-center">
-                  <h3 className="text-left text-xl font-bold mb-2">
-                    Movimientos
-                  </h3>
-                  <div className="flex flex-col gap-2">
-                    {translatedPokemon.moves?.map((poke, index) => (
-                      <p key={index} className="text-left">
-                        {poke}
-                      </p>
-                    ))}
+  } else {
+    return (
+      <div className="flex w-screen h-screen items-center justify-center bg-slate-100">
+        <div>
+          {pokemon && translatedPokemon ? (
+            <div className="card card-side p-4 bg-base-100 shadow-xl max-sm:flex-col">
+              <figure>
+                <img
+                  src={pokemon?.image}
+                  className="w-64 ml-10 max-sm:ml-0"
+                  alt="Movie"
+                />
+              </figure>
+              <div className="card-body mr-16 max-sm:w-full">
+                <h1 className="card-title text-4xl ml-3 max-sm:justify-center max-sm:text-2xl">
+                  {translatedPokemon.name.toUpperCase()}
+                </h1>
+                <div className="p-4 flex flex-row gap-6 max-sm:justify-center">
+                  <div className="max-sm:justify-center">
+                    <h3 className="text-left text-xl font-bold mb-2 max-sm:mr-6">
+                      Movimientos
+                    </h3>
+                    <div className="flex flex-col gap-2">
+                      {translatedPokemon.moves?.map((poke, index) => (
+                        <p key={index} className="text-left">
+                          {poke}
+                        </p>
+                      ))}
+                    </div>
+                  </div>
+                  <div>
+                    <h3 className="text-left text-xl font-bold mb-2 max-sm:ml-1">
+                      Tipos
+                    </h3>
+                    <div className="flex flex-col max-sm:ml-2">
+                      {translatedPokemon.types?.map((poke, index) => (
+                        <p key={index} className="text-left max-sm:text-left">
+                          {poke}
+                        </p>
+                      ))}
+                    </div>
                   </div>
                 </div>
-                <div>
-                  <h3 className="text-left text-xl font-bold mb-2">Tipos</h3>
-                  <div className="flex flex-col gap-2">
-                    {translatedPokemon.types?.map((poke, index) => (
-                      <p key={index} className="text-left">
-                        {poke}
-                      </p>
-                    ))}
-                  </div>
+                <div className="card-actions justify-start ml-3 max-sm:justify-center">
+                  <button
+                    className="btn btn-primary w-56 max-sm:w-64"
+                    onClick={changePokemon}>
+                    OBTEN OTRO POKEMON
+                  </button>
                 </div>
-              </div>
-              <div className="card-actions justify-start ml-3 max-sm:justify-center">
-                <button
-                  className="btn btn-primary w-56"
-                  onClick={changePokemon}>
-                  OBTEN OTRO POKEMON
-                </button>
               </div>
             </div>
-          </div>
-        ) : (
-          <div className="flex items-center justify-center h-screen w-screen bg-slate-100">
-            <button
-              className="btn btn-primary w-64 bg-white text-violet-800 hover:text-white"
-              onClick={changePokemon}>
-              OBTEN UN POKEMON
-            </button>
-          </div>
-        )}
+          ) : (
+            <div className="flex items-center justify-center h-screen w-screen bg-slate-100">
+              <button
+                className="btn btn-primary w-64 bg-white text-violet-800 hover:text-white"
+                onClick={changePokemon}>
+                OBTEN UN POKEMON
+              </button>
+            </div>
+          )}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
